21b46cd239d68d075ecb150a1f5a92de55a90519
Diego Wang <wangdiego1119@gmail.com>
2026-02-09 00:01:45 +0800

    fix the frontend disconnection

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "../squishfile/frontend/dist",
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
     proxy: {
